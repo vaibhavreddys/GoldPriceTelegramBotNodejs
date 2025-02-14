0231d29ae511ca7944a9332c365907bdab5c85bd
Vaibhav Reddy <vaibhavfury@gmail.com>
2025-02-15 01:29:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const bot = require("./bot");
+
+module.exports = async (req, res) => {
+  if (req.method === "POST") {
+    await bot.handleUpdate(req.body, res);
+  } else {
+    res.status(200).json({ status: "ok", message: "Server is running" });
+  }
+};
