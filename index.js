99a9fab8d3d5469c9a301ca5bd217d7bcdd5b2c2
Vaibhav Reddy <vaibhavfury@gmail.com>
2025-02-15 01:39:25 +0530

    Update index.js
    Thanking Vercel for their offerings :)

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ module.exports = async (req, res) => {
   if (req.method === "POST") {
     await bot.handleUpdate(req.body, res);
   } else {
-    res.status(200).json({ status: "ok", message: "Server is running" });
+    res.status(200).json({ status: "ok", message: "Server is running, thanks Vercel, ILY." });
   }
 };
